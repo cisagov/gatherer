6cfb3496337fae7b66e3026ef0c6c09d4b52353a
Jeremy Frasier <jeremy.frasier@gwe.cisa.dhs.gov>
2026-01-05 12:45:53 -0500

    Prefer f-strings
    These changes get rid of errors from our pyupgrade pre-commit hook.

--- src/fed_hostnames.py
+++ src/fed_hostnames.py
@@ -86,7 +86,7 @@ def main():
         db = db_from_config(db_creds_file)
     except OSError:
         logging.critical(
-            "Database configuration file {} does not exist".format(db_creds_file),
+            f"Database configuration file {db_creds_file} does not exist",
             exc_info=True,
         )
         return 1
@@ -108,19 +108,19 @@ def main():
         return 1
     except pymongo.errors.ConnectionError:
         logging.critical(
-            "Unable to connect to the database server in {}".format(db_creds_file),
+            f"Unable to connect to the database server in {db_creds_file}",
             exc_info=True,
         )
         return 1
     except pymongo.errors.InvalidName:
         logging.critical(
-            "The database in {} does not exist".format(db_creds_file), exc_info=True
+            f"The database in {db_creds_file} does not exist", exc_info=True
         )
         return 1
 
     # Get all Federal organizations
     fed_orgs = get_all_descendants(db, "FEDERAL")
-    logging.debug("Federal orgs are {}".format(fed_orgs))
+    logging.debug(f"Federal orgs are {fed_orgs}")
 
     # Get all Federal hosts with open ports that indicate a possible web or
     # email server (latest scan only)...
@@ -150,7 +150,7 @@ def main():
     with open(args["--output-file"], "w") as file:
         for host in fed_hosts:
             file.write("{},{}\n".format(host["hostname"], host["owner"]))
-            logging.debug("Federal host {}".format(host))
+            logging.debug(f"Federal host {host}")
 
 
 if __name__ == "__main__":
